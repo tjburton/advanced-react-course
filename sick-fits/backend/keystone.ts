4a088c16c7df3f35c65f289fff21f9499bcbb402
Terence Burton <terence.burton@sky.uk>
2024-04-09 14:29:37 +0100

    added product and productImages schema

--- sick-fits/backend/keystone.ts
+++ sick-fits/backend/keystone.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config';
-import {config, createSchema} from '@keystone-next/keystone/schema';
-import {createAuth} from '@keystone-next/auth';
-import {withItemData, statelessSessions} from '@keystone-next/keystone/session';
-import {User} from './schemas/User';
-import {Product} from './schemas/Product';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import { createAuth } from '@keystone-next/auth';
+import { withItemData, statelessSessions } from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL || 'mongodb://localhost/keystone-sick-fits-tutorial';
 
@@ -12,7 +14,7 @@ const sessionConfig = {
   secret: process.env.COOKIE_SECRET,
 };
 
-const {withAuth} = createAuth({
+const { withAuth } = createAuth({
   listKey: 'User',
   identityField: 'email',
   secretField: 'password',
@@ -33,16 +35,22 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
-      // TODO: Add data seeding here
+      async onConnect(keystone) {
+        console.log('Connected to the database!');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       // TODO: Scheme items go in here
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       // TODO: Show the UI for only people who pass this test
-      isAccessAllowed: ({session}) => !!session?.data,
+      isAccessAllowed: ({ session }) => !!session?.data,
     },
     session: withItemData(statelessSessions(sessionConfig), {
       User: 'id',
--- sick-fits/backend/schemas/Product.ts
+++ sick-fits/backend/schemas/Product.ts
@@ -1,21 +1,30 @@
-import {integer, select, text} from '@keystone-next/fields';
-import {list} from '@keystone-next/keystone/schema';
+import { integer, relationship, select, text } from '@keystone-next/fields';
+import { list } from '@keystone-next/keystone/schema';
 
 export const Product = list({
   // TODO: Access
   fields: {
-    name: text({isRequired: true}),
-    description: text({ui: {displayMode: 'textarea'}}),
+    name: text({ isRequired: true }),
+    description: text({ ui: { displayMode: 'textarea' } }),
+    photo: relationship({
+      ref: 'ProductImage.product',
+      ui: {
+        displayMode: 'cards',
+        cardFields: ['image', 'altText'],
+        inlineCreate: { fields: ['image', 'altText'] },
+        inlineEdit: { fields: ['image', 'altText'] },
+      },
+    }),
     status: select({
       options: [
-        {label: 'Draft', value: 'DRAFT'},
-        {label: 'Available', value: 'AVAILABLE'},
-        {label: 'Unavailable', value: 'UNAVAILABLE'},
+        { label: 'Draft', value: 'DRAFT' },
+        { label: 'Available', value: 'AVAILABLE' },
+        { label: 'Unavailable', value: 'UNAVAILABLE' },
       ],
       defaultValue: 'DRAFT',
       ui: {
         displayMode: 'segmented-control',
-        createView: {fieldMode: 'hidden'},
+        createView: { fieldMode: 'hidden' },
       },
     }),
     price: integer(select),
